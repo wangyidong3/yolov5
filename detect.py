3b1f7213fc8d962865b454e435fa7e09528b7362
wangyidong3 <wangyidong3@163.com>
2020-08-24 10:10:04 +1200

    detect for sbd

--- detect.py
+++ detect.py
@@ -8,15 +8,126 @@ from pathlib import Path
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
+import numpy as np
 from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadImages, letterbox
 from utils.general import (
-    check_img_size, non_max_suppression, apply_classifier, scale_coords,
-    xyxy2xywh, plot_one_box, strip_optimizer, set_logging)
+    check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, plot_one_box, strip_optimizer)
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
+
+def detect_yolov5(cv_img):
+    print("yolov5")
+    out, source, weights, save_txt, imgsz = \
+        './inference/output', '', 'yolov5s.pt',  '', 640
+    opt_augment, opt_agnostic_nms, opt_iou_thres, opt_classes = '','',0.5, ''
+    opt_conf_thres = 0.4
+    
+    # webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # Initialize
+    device = select_device("0") # opt.device
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+    print("half:",half)
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+
+    view_img = True
+    cudnn.benchmark = True  # set True to speed up constant image size inference
+    save_img = True
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+
+    im0s = cv_img
+    # Padded resize
+    img = letterbox(cv_img, new_shape=640)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Inference
+    t1 = time_synchronized()
+    pred = model(img, augment=None)[0]
+
+    # Apply NMS
+    pred = non_max_suppression(pred, opt_conf_thres, opt_iou_thres, classes=opt_classes, agnostic=opt_agnostic_nms)
+    t2 = time_synchronized()
+
+    
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+
+        s, im0 = '', im0s
+
+        save_path = '/home/don/test/yolov5/inference/output/result.jpg'    #str(Path(out) / Path(p).name)
+        
+        s += '%gx%g ' % img.shape[2:]  # print string
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if det is not None and len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+            # Write results
+            for *xyxy, conf, cls in det:
+                print("\n>>>>>>>>>>>>>>>>>>:", xyxy, conf,cls)
+
+                if save_img or view_img:  # Add bbox to image
+                    label = '%s %.2f' % (names[int(cls)], conf)
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
+        # Print time (inference + NMS)
+        print('%sDone. (%.3fs)' % (s, t2 - t1))
+    '''
+        # Stream results
+        if view_img:
+            cv2.imshow("image", im0)
+            if cv2.waitKey(1) == ord('q'):  # q to quit
+                raise StopIteration
+
+        # Save results (image with detections)
+        print(save_path)
+        if save_img:
+            cv2.imwrite(save_path, im0)
+
+
+    if save_txt or save_img:
+        print('Results saved to %s' % Path(out))
+        if platform == 'darwin' and not opt.update:  # MacOS
+            os.system('open ' + save_path)
+    '''
+    print('Done. (%.3fs)' % (time.time() - t0))    
+    return 
 
 def detect(save_img=False):
     out, source, weights, view_img, save_txt, imgsz = \
@@ -24,7 +135,6 @@ def detect(save_img=False):
     webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
 
     # Initialize
-    set_logging()
     device = select_device(opt.device)
     if os.path.exists(out):
         shutil.rmtree(out)  # delete output folder
@@ -102,7 +212,8 @@ def detect(save_img=False):
                     s += '%g %ss, ' % (n, names[int(c)])  # add to string
 
                 # Write results
-                for *xyxy, conf, cls in reversed(det):
+                for *xyxy, conf, cls in det:
+                    print("\n>>>>>>>>>>>>>>>>>>:", xyxy, conf,cls)
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         with open(txt_path + '.txt', 'a') as f:
@@ -140,12 +251,18 @@ def detect(save_img=False):
 
     if save_txt or save_img:
         print('Results saved to %s' % Path(out))
-        if platform.system() == 'Darwin' and not opt.update:  # MacOS
+        if platform == 'darwin' and not opt.update:  # MacOS
             os.system('open ' + save_path)
 
     print('Done. (%.3fs)' % (time.time() - t0))
 
-
+'''
+if __name__ == '__main__':
+    with torch.no_grad():
+        cv_img = cv2.imread("/home/don/test/yolov5/inference/images/bus.jpg")
+        a = detect_yolov5(cv_img)
+        print(a)
+'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
@@ -162,6 +279,7 @@ if __name__ == '__main__':
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--update', action='store_true', help='update all models')
     opt = parser.parse_args()
+    print(opt.augment)
     print(opt)
 
     with torch.no_grad():
